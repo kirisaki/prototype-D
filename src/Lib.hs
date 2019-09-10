037571d5e5b1d21a9785618259106650bc06f654
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-09-10 18:06:27 +0900

    fis: parse if

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,8 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Void
 import Data.Functor
+import qualified Data.Set as SE
+import Control.Monad
 
 data Expr
   = Var String
@@ -27,32 +29,42 @@ true = string "True" $> Bool True
 false :: Parser Expr
 false = string "False" $> Bool True
 
+reserved :: [String]
+reserved =
+  [ "if"
+  , "then"
+  , "else"
+  ]
+
 if_ :: Parser Expr
-if_ = If <$> (string "if" *> term) <*> (string "then" *> term) <*> (string "else" *> term)
+if_ = If <$> (string "if" *> expr) <*> (string "then" *> expr) <*> (string "else" *> expr)
 
 var :: Parser Expr
-var = Var <$> some (alphaNumChar <|> symbolChar)
+var = do
+  sym <- some (alphaNumChar <|> symbolChar)
+  when (sym `elem` reserved) (failure Nothing SE.empty)
+  pure $ Var sym
 
 lambda :: Parser Expr
 lambda = Lambda <$> (char '\\' >> space *> some letterChar) <*> (space >> string "->" >> space *> expr)
 
-apply :: Parser (Expr -> Expr -> Expr)
-apply = space $> Apply
 
 term :: Parser Expr
-term = between space space $ if_ <|> true <|> false <|> try var <|> try lambda <|> parens expr
+term = between space space $ true <|> false <|> var <|> lambda <|> parens expr
 
-expr :: Parser Expr
-expr = do
-  space
+apply :: Parser Expr
+apply = do
   t <- term
-  loop t <* space
+  loop t
   where
-    loop e = loop' e <|> pure e
+    loop e = try (loop' e) <|> pure e
     loop' lhs = do
-      op <- apply
+      op <- space $> Apply
       rhs <- term
       loop $ op lhs rhs
+
+expr :: Parser Expr
+expr = between space space $ if_ <|> apply <|> term
 
 parens :: Parser Expr -> Parser Expr
 parens = between (char '(' <* space) (space *> char ')')
@@ -61,5 +73,5 @@ someFunc :: IO ()
 someFunc = do
   parseTest expr "(\\x -> add x x)"
   parseTest expr "(\\x -> add x x)12"
-  parseTest expr "x y z"
-  parseTest expr "if True then x else y"
+  parseTest expr "x y z "
+  parseTest expr "if f x then y else z"
